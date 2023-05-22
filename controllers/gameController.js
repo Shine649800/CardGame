42d4776552c24368170993491d488ee8c982ffa9
zu3x <zu3x@fresnou.org>
2023-05-22 11:11:55 -0700

    Sixteen commit, struggling with displaying enemy according to stage

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -1,12 +1,21 @@
 const {Deck, Card, Enemy, EnemyCards, Stage, StageEnemies} = require('../models');
 const currentDeck = [];
-const enemy = [];
+const currentEnemy = [];
 const enemyCards =[];
 
 module.exports.renderGame = async function(req,res){
     const stage = await Stage.findByPk(req.params.stageId, {
-        include: ['enemy']
-    }
+        include: {
+            model: Enemy,
+            as: 'enemy',
+            // include: [
+            //     {
+            //         model: EnemyCards,
+            //         as: 'enemycards'
+            //     }
+            // ]
+        }
+        }
     );
     res.render('game', {stage});
 }
