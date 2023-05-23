786f6a326f5baf4932ec55b8533bb06cc9178379
zu3x <zu3x@fresnou.org>
2023-05-23 11:31:26 -0700

    Seventeen commit, Display enemy works, now working on game functions.

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -1,21 +1,16 @@
-const {Deck, Card, Enemy, EnemyCards, Stage, StageEnemies} = require('../models');
-const currentDeck = [];
-const currentEnemy = [];
-const enemyCards =[];
+const {Deck, Card, Enemy, EnemyCards, Stage} = require('../models');
 
 module.exports.renderGame = async function(req,res){
     const stage = await Stage.findByPk(req.params.stageId, {
         include: {
             model: Enemy,
             as: 'enemy',
-            // include: [
-            //     {
-            //         model: EnemyCards,
-            //         as: 'enemycards'
-            //     }
-            // ]
         }
         }
     );
-    res.render('game', {stage});
+    let currentDeck = [];
+    let enemyCards =[];
+    await EnemyCards.findByPk(req.params.enemyId);
+    enemyCards.push(EnemyCards);
+    res.render('game', {stage, enemyCards});
 }
